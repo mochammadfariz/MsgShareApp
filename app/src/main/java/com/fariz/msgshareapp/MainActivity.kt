1cf03692f969f08cd82e7074b019c1889cb1da66
mochammadfariz <mochammadfariz11@gmail.com>
2019-09-20 22:50:38 +0700

    Section Two, Video Two

--- app/src/main/java/com/fariz/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/fariz/msgshareapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.fariz.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 }
